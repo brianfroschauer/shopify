eb1559d6b12cf7547a541d017c79c599df0cd548
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-05-20 18:13:07 -0300

    Add catalog implementation

--- src/main/java/com/acs/shopify/model/Catalog.java
+++ src/main/java/com/acs/shopify/model/Catalog.java
@@ -1,11 +1,38 @@
 package com.acs.shopify.model;
 
-import javax.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Author: brianfroschauer
  * Date: 2019-05-20
  */
+@Data
+@NoArgsConstructor
 @Entity(name = "catalog")
 public class Catalog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "total_items", nullable = false)
+    private Integer totalItems = 0;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private Set<CatalogItem> items;
+
+    public void addItem(CatalogItem item) {
+        items.add(item);
+        totalItems += 1;
+    }
+
+    public void removeItem(CatalogItem item) {
+        items.remove(item);
+        totalItems -= 1;
+    }
 }
