f6de711aac1d5e28257981c5f481a9e1ac99a156
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-05-20 17:01:32 -0300

    Add implementation

--- src/main/java/com/acs/shopify/controller/CartController.java
+++ src/main/java/com/acs/shopify/controller/CartController.java
@@ -1,8 +1,110 @@
 package com.acs.shopify.controller;
 
+import com.acs.shopify.dto.cart.ResponseCart;
+import com.acs.shopify.dto.cartItem.ResponseCartItem;
+import com.acs.shopify.dto.product.RequestProduct;
+import com.acs.shopify.dto.product.ResponseProduct;
+import com.acs.shopify.model.Cart;
+import com.acs.shopify.model.Product;
+import com.acs.shopify.service.CartService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Author: brianfroschauer
  * Date: 2019-05-20
  */
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    private final CartService cartService;
+    private final ModelMapper mapper;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+        this.mapper = new ModelMapper();
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<ResponseCart> getCart(
+            @PathVariable Long cartId) {
+
+        final Cart cart = cartService.getCart(cartId);
+
+        final ResponseCart responseCart = mapper
+                .map(cart, ResponseCart.class);
+
+        return ResponseEntity.ok(responseCart);
+    }
+
+    @GetMapping("/{cartId}/items")
+    public ResponseEntity<List<ResponseCartItem>> getItems(
+            @PathVariable Long cartId) {
+
+        final List<ResponseCartItem> all = cartService.getItems(cartId).stream()
+                .map(customer -> mapper.map(customer, ResponseCartItem.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(all);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseProduct> addToCart(
+            @RequestBody @Valid RequestProduct requestProduct) {
+
+        final Product product = mapper.map(requestProduct, Product.class);
+
+        cartService.addToCart(product);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Cart> addToCart(
+            @RequestBody @Valid RequestProduct requestProduct,
+            @RequestParam(value = "amount") Integer amount) {
+
+        final Product product = mapper.map(requestProduct, Product.class);
+
+        cartService.addToCart(product, amount);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{cartId}/items/{itemId}")
+    public ResponseEntity remove(
+            @PathVariable Long cartId,
+            @PathVariable Long itemId) {
+
+        cartService.remove(cartId, itemId);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping("/{cartId}/items/{itemId}")
+    public ResponseEntity remove(
+            @PathVariable Long cartId,
+            @PathVariable Long itemId,
+            @RequestParam(value = "amount") Integer amount) {
+
+        cartService.remove(cartId, itemId, amount);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{cartId}")
+    public ResponseEntity clear(
+            @PathVariable Long cartId) {
+
+        cartService.clear(cartId);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/acs/shopify/controller/CustomerController.java
+++ src/main/java/com/acs/shopify/controller/CustomerController.java
@@ -1,8 +1,98 @@
 package com.acs.shopify.controller;
 
+import com.acs.shopify.dto.customer.RequestCustomer;
+import com.acs.shopify.dto.customer.ResponseCustomer;
+import com.acs.shopify.model.Customer;
+import com.acs.shopify.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Author: brianfroschauer
  * Date: 2019-05-20
  */
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+    private final ModelMapper mapper;
+
+    @Autowired
+    private CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+        this.mapper = new ModelMapper();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseCustomer> getOne(
+            @PathVariable Long id) {
+
+        final Customer one = customerService.findOne(id);
+
+        final ResponseCustomer responseCustomer = mapper
+                .map(one, ResponseCustomer.class);
+
+        return ResponseEntity.ok(responseCustomer);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseCustomer>> getAll() {
+
+        final List<ResponseCustomer> all = customerService.findAll().stream()
+                .map(customer -> mapper.map(customer, ResponseCustomer.class))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(all);
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseCustomer> save(
+            @RequestBody @Valid RequestCustomer requestCustomer) {
+
+        final Customer customer = mapper.map(requestCustomer, Customer.class);
+
+        final Customer saved = customerService.save(customer);
+
+        final URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(saved.getId()).toUri();
+
+        final ResponseCustomer responseCustomer = mapper
+                .map(saved, ResponseCustomer.class);
+
+        return ResponseEntity.created(location).body(responseCustomer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseCustomer> update(
+            @PathVariable Long id,
+            @RequestBody @Valid RequestCustomer requestCustomer) {
+
+        final Customer customer = mapper.map(requestCustomer, Customer.class);
+
+        final Customer updated = customerService.update(id, customer);
+
+        final ResponseCustomer responseCustomer = mapper
+                .map(updated, ResponseCustomer.class);
+
+        return ResponseEntity.ok(responseCustomer);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseCustomer> delete(@PathVariable Long id) {
+
+        customerService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
