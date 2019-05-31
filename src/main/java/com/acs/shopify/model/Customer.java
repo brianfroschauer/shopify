f462d105566d00ace51798b75d0acf28f0610190
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-05-31 01:34:27 -0300

    delete notnull constrains

--- src/main/java/com/acs/shopify/model/Customer.java
+++ src/main/java/com/acs/shopify/model/Customer.java
@@ -16,19 +16,19 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id")
     private Long id;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "username", nullable = false, unique = true)
+    @Column(name = "username", unique = true)
     private String username;
 
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
 
     public Customer(String firstName, String lastName, String username, String password) {
